6b0db36cbf804509a680b09080ab8b7c73b77085
luchang <luchang@lab108.fudan.edu.cn>
2017-12-05 18:14:40 +0800

    add parse NYTimes by day

--- nytimes_crawler.py
+++ nytimes_crawler.py
@@ -1,12 +1,64 @@
-from settings.dataset_conf import DatasetConfiguration
-from link.nytimes_link import NytimesLinkFetcher
+import os.path
+import json
 
+from dateutil.relativedelta import relativedelta
+
+from settings.dataset_conf import DatasetConfiguration
+from article.nytimes_article import NytimeArticleFetcher
+
+
+def parse_nytimes(fetcher):
+    config = fetcher.config
+    current_date = config.start_date
+    while current_date < config.end_date:
+        storage_path = fetcher._get_storage_path(config.path,
+                                                 str(current_date.year), str(current_date.month))
+        articles_path = os.path.join(storage_path, 'articles')
+        with open(articles_path, encoding='utf-8') as articles_file:
+            articles = json.load(articles_file)
+            day_articles = list()
+            day_titles = list()
+            for i in range(0, 31):
+                day_articles.append([])
+                day_titles.append([])
+            for article in articles['articles']:
+                day = int(article['published_date'][-2:])
+                day_articles[day - 1].append(article)
+                day_titles[day - 1].append(article['title'] + '\n')
+
+            day_step = relativedelta(day=1)
+            month_start_date = current_date
+            month_end_date = month_start_date + relativedelta(months=1)
+            month_end_date.replace(day=1)
+            if month_end_date > config.end_date:
+                month_end_date = config.end_date
+
+            month_current_date = month_start_date
+            while month_start_date < month_end_date:
+                day = month_current_date.day
+                day_path = os.path.join(storage_path, str(day))
+                if not os.path.isdir(day_path):
+                    os.mkdir(day_path)
+
+                day_titles_path = os.path.join(day_path, 'titles')
+                with open(day_titles_path, mode='w', encoding='utf-8') as day_titles_file:
+                    day_titles_file.writelines(day_titles[day - 1])
+                day_articles_path = os.path.join(day_path, 'articles')
+                with open(day_articles_path, mode='w', encoding='utf-8') as day_articles_file:
+                    json.dump({
+                        'number': len(day_articles[day - 1]),
+                        'articles': day_articles[day - 1]
+                    }, day_articles_file, indent=4)
+
+                month_current_date += day_step
+
+        current_date += config.step
 
 if __name__ == '__main__':
     config = DatasetConfiguration()
     config.load('./settings/nytimes.cfg')
 
-    nytimes_link_fetcher = NytimesLinkFetcher(config)
-    api_urls, date = nytimes_link_fetcher.next()
-    links = nytimes_link_fetcher.fetch(api_urls)
-    print(links)
+    nytime_article_fetcher = NytimeArticleFetcher(config)
+    nytime_article_fetcher.fetch()
+
+    parse_nytimes(nytime_article_fetcher)
